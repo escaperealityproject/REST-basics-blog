3a320c39f882b8c98125beff90c283182591cc47
aniket <escaperealityproject@gmail.com>
2018-08-07 22:29:36 +0530

    Added edit and update route, styled forms and installed method-override

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express"),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
   app = express();
 
 //APP CONFIG
@@ -15,6 +16,8 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(methodOverride("_method"));
 
 //MONGOOSE CONFIG
 
@@ -68,6 +71,49 @@ app.post("/blogs", function(req, res) {
       res.render("new");
     } else {
       res.redirect("/blogs");
+    }
+  })
+});
+
+//SHOW ROUTE
+
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {
+        blog: foundBlog
+      });
+
+    }
+  })
+});
+
+//EDIT ROUTE
+
+app.get("/blogs/:id/edit",function(req,res){
+  Blog.findById(req.params.id,function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    }else{
+      res.render("edit",{blog : foundBlog});
+    }
+  })
+});
+
+//UPDATE ROUTE
+
+app.put("/blogs/:id",function(req,res){
+  Blog.findByIdAndUpdate(req.params.id,{
+    title: req.body.title,
+    image: req.body.image,
+    body: req.body.body
+  },function(err,updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    }else{
+      res.redirect("/blogs/"+req.params.id);
     }
   })
 })
